2572b1a816a0d4bb88b743533bed2f86f1009cfc
Naiem Islam <naiem4288@gmail.com>
2023-08-13 18:12:20 +0600

    feat: Implemented addItemToCart feature

--- components/shared/ProductCard/ProductCard.js
+++ components/shared/ProductCard/ProductCard.js
@@ -1,18 +1,19 @@
+'use client'
+
+import { addItemToCart } from '@redux/slices/cartSlice';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
 
-const laptop =
-  'https://template66564.motopreview.com/mt-demo/66500/66564/mt-content/uploads/2018/07/mt-1522_products_img01.jpg';
-const tablet =
-  'https://template66564.motopreview.com/mt-demo/66500/66564/mt-content/uploads/2018/07/mt-1522_products_img02.jpg';
-const speaker =
-  'https://template66564.motopreview.com/mt-demo/66500/66564/mt-content/uploads/2018/07/mt-1522_products_img03.jpg';
-const phone =
-  'https://template66564.motopreview.com/mt-demo/66500/66564/mt-content/uploads/2018/07/mt-1522_products_img04.jpg';
 
 const ProductCard = ({ product }) => {
+  const dispatch = useDispatch();
   const { thumbnail, images, title, price, id } = product;
- console.log(product.id)
+ 
+  const handleAddToCart = (product) => {
+    dispatch(addItemToCart(product))
+  }
+
 
   return (
     <div className="border p-2  max-w-[300px] text-center m-2">
@@ -29,7 +30,7 @@ const ProductCard = ({ product }) => {
           <p className="font-bold text-gray-500 text-lg">$ {price}</p>
         </div>
         <div className="mt-3">
-          <button className="bg-blue-700 py-2 px-4 text-white font-semibold hover:bg-blue-800">
+          <button onClick={() => handleAddToCart(product)} className="bg-blue-700 py-2 px-4 text-white font-semibold hover:bg-blue-800">
             ADD TO CART
           </button>
         </div>
