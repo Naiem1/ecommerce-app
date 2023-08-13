97569a25103497c9cfc049b9fec678061821b241
Naiem Islam <naiem4288@gmail.com>
2023-08-13 18:44:24 +0600

    feat: Implemented redux persist for persist all data

--- redux/store/store.js
+++ redux/store/store.js
@@ -1,13 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import productsReducer from '../slices/productsSlice';
 import cartReducer from "@redux/slices/cartSlice";
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  
+}
 
-const store = configureStore({
-  reducer: {
-    products: productsReducer,
-    cart: cartReducer,
-  }
+const rootReducer  = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
 })
 
-export default store;
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer
+})
+
+
+const persistor = persistStore(store);
+
+export { store, persistor };
