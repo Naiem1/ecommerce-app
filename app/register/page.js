52859261819d8ae65ae47a9fd6429249f1b128f5
Naiem Islam <naiem4288@gmail.com>
2023-08-14 16:14:31 +0600

    manged register and login local state

--- app/register/page.js
+++ app/register/page.js
@@ -1,22 +1,49 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+
 const Register = () => {
+  const [credential, setCredential] = useState({
+    phone: 0,
+    password: '',
+    name: '',
+  });
+
+  const handleRegister = (e) => {
+    setCredential({
+      ...credential,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(credential);
+  }
+
+  console.log(credential);
+
   return (
     <div className="mt-10">
       <div className="w-full bg-[#3C1FF4]  py-12">
         <p className="w-9/12 m-auto pl-4 text-white text-lg">register</p>
       </div>
-      
+
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-lg">
           <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
-            Sign Up
+            Sign up
           </h1>
 
           <form
+            onSubmit={handleFormSubmit}
             action=""
             className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg border border-blue-700 sm:p-6 lg:p-8"
           >
             <p className="text-center text-lg font-medium">
-              Sign Up to New account
+              Sign up to New account
             </p>
 
             <div>
@@ -26,6 +53,7 @@ const Register = () => {
                   className="w-full rounded-lg border-gray-200 outline-blue-700 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter phone number"
                   name="phone"
+                  onChange={handleRegister}
                 />
               </div>
             </div>
@@ -37,6 +65,7 @@ const Register = () => {
                   className="w-full rounded-lg border-gray-200 outline-blue-700 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter password"
                   name="password"
+                  onChange={handleRegister}
                 />
               </div>
             </div>
@@ -48,6 +77,7 @@ const Register = () => {
                   className="w-full rounded-lg border-gray-200 outline-blue-700 p-4 pe-12 text-sm shadow-sm"
                   placeholder="Enter your name"
                   name="name"
+                  onChange={handleRegister}
                 />
               </div>
             </div>
@@ -61,9 +91,9 @@ const Register = () => {
 
             <p className="text-center text-sm text-gray-500">
               Have an account?
-              <a className="underline" href="">
-                Login in
-              </a>
+              <Link className="underline" href="/login">
+                  Login in
+              </Link>
             </p>
           </form>
         </div>
